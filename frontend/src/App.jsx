3b67915787d1784ef0d0fcd4c1bc5c011c6926d6
Hussnat <209363827+Hussnat@users.noreply.github.com>
2025-11-14 21:39:24 +0500

    Fix route issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <Routes>
         {/* Public Routes */}
+          <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
